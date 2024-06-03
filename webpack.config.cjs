2a880a818a78837b4023ca36a5d375794fb59033
Ilya Kreymer <ikreymer@users.noreply.github.com>
2024-06-03 15:40:33 -0700

    Dependency Updates (0.4.4) (#10)
    * update wabac.js to 2.18.1 / wombat 3.7.4
    * bump ipld/car and ipld/unixfs
    * bump webpack
    * more specific 
    * add 'library' to webpack to ensure exports are not optimized out

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,31 @@
 /*eslint-env node */
 
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const BANNER_TEXT = `'[name].js is part of the ArchiveWeb.page system (https://archiveweb.page) Copyright (C) 2020-${new Date().getFullYear()}, Webrecorder Software. Licensed under the Affero General Public License v3.'`;
 
 
 module.exports = {
+  target: "webworker",
+  entry: {
+    "main": "./src/index.js",
+  },
   output: {
     filename: "sw.js",
+    library: {
+      type: "self"
+    }
   },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ]
+  },
+
   plugins: [
     new webpack.NormalModuleReplacementPlugin(
       /^node:*/,
